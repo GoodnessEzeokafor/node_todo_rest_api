ad1ba54f3cd19e544f6a69d89d1ca54abc1d0fa2
Ezeokafor <gootech442@gmail.com>
2018-11-24 14:12:07 +0100

    work on postman

--- server/server.js
+++ server/server.js
@@ -15,13 +15,24 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res)=>{
     var todo = new Todo({
         text:req.body.text,
-
+        completed:req.body.completed,
+        completedAt:req.body.completedAt
     });
         todo.save().then((doc)=>{
             res.send(doc)
         }).catch(e=>res.status(400).send(e));
 });
-
+app.get('/todos', (req,res)=>{
+    Todo.find()
+        .then((todos)=>{
+            res.send({
+                todos
+            })
+        })
+        .catch((e)=>{
+            res.status(404).send(e);
+        })
+});
 app.listen(3000, ()=>{
     console.log('Started on port 3000');
 });
